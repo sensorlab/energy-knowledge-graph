f071f89253e894e16c110e22620532c8e604f76a
vHanzel <vid.hanzel@gmail.com>
2024-01-04 09:51:17 +0100

    Fixed error in parser and added dataset info

--- scripts/parsers/HUE_parser.py
+++ scripts/parsers/HUE_parser.py
@@ -1,6 +1,15 @@
 import pandas as pd
 from helper_functions import watts2kwh, save_to_pickle
 
+######################DATASET INFO#########################################
+# sampling rate: 1 hour
+# unit: kWh
+# households: 28
+# no submeter data
+# Location: Canada
+# Source: https://dataverse.harvard.edu/dataset.xhtml?persistentId=doi:10.7910/DVN/N3HGRN
+
+
 def to_dict(df):
     return {"aggregate" : pd.DataFrame(df)}
 
@@ -11,9 +20,11 @@ def parse_HUE(data_path, save_path):
     # wH -> kWh
     residentials["energy"] = residentials["energy"] /1000
     residentials = residentials.copy()
-    
-    # save each house in a separate dataframe
-    df_dict = {"HUE_"+str(id): to_dict(residentials["energy"]) for id, df_group in residentials.groupby('residential_id')}
+        
+    # Create a dictionary with the data for each house
+    data = {}
+    for id in residentials["residential_id"].unique():
+        data["HUE_" + str(id)] = {"aggregate" : residentials.loc[residentials["residential_id"] == id, "energy"]}
 
 
-    save_to_pickle(df_dict, save_path)
+    save_to_pickle(data, save_path)
